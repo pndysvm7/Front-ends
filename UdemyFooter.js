b591347bf4e083b81e5c58d4240a5f0642c37dc5
pndysvm7 <73978138+pndysvm7@users.noreply.github.com>
2022-02-07 10:53:53 +0530

    Create UdemyFooter.js

---
+++ UdemyFooter.js
@@ -0,0 +1,96 @@
+ <div>
+            <div className="flex flex-grow flex-col med:flex-col large:flex-row lap:flex-row 2xl:flex-row med:justify-between large:justify-between lap:justify-between 2xl:justify-between items-center bg-black py-5  large:px-8 lap:px-8 2xl:px-8 space-y-4 med:space-y-0 large:space-y-0 lap:space-y-0 2xl:space-y-0 border-b border-slate-100 ">
+
+                <div className="w-full py-2 tab:flex tab:justify-start  mob:flex mob:justify-start">
+                    <p className=" flex items-center justify-center text-lg font-bold text-white med:px-5  mx-5  large:ml-2 lap:mx-5 2xl:mx-5">Top companies choose  to build in-demand career skills.</p>
+
+                </div>
+
+
+                <div className="w-full py-2 tab:flex tab:justify-start   mob:flex mob:justify-start">
+                    <div className=" flex flex-col  tab:flex-col med:flex-row large:flex-row lap:flex-row 2xl:flex-row items-center justify-start med:justify-center large:justify-end lap:justify-end 2xl:justify-end text-lg font-bold text-white large:px-2 lap:px-2 2xl:px-2 mx-5">
+
+
+                        <img className=" flex justify-start px-2 large:px-2 lap:px-3 2xl:px-3" src="/nasdaq-light.svg" />
+
+
+
+
+
+
+
+
+                        <img className="px-2 large:px-2 lap:px-3 2xl:px-3" src='/volkswagen-light.svg' />
+                        <img className="px-2 large:px-2 lap:px-3 2xl:px-3" src='/box-light.svg' />
+                        <img className="px-2 large:px-2 lap:px-3 2xl:px-3" src="/eventbrite-light.svg" />
+                        <img className="px-2 large:px-2 lap:px-3 2xl:px-3" src='/netapp-light.svg' />
+
+                    </div>
+
+                </div>
+
+
+
+
+
+            </div>
+
+
+
+            <div className="bg-black ">
+
+
+                <div className="text-white px-6 py-4 ">
+
+
+
+                    <div className="mob:py-4 tab:py-4 py-5 px-4 grid tab:grid-cols-2 med:grid-cols-3 large:grid-cols-3 lap:grid-cols-5 2xl:grid-cols-5">
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">Udemy Business</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">Teach on Udemy</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start tab:justify-start mob:text-sm tab:text-sm">Get the app</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">About Us</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">Contact Us</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">Careers</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">Blog</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">Help and Support</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">Affiliate</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">Investors</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">Terms</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">Privacy policy</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">Cookie Setting</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start mob:text-sm tab:justify-start tab:text-sm">Sitemap</div>
+
+                        <div className="px-2 py-2 flex justify-start items-center mob:justify-start
+                        tab:justify-start mob:text-sm tab:text-sm">Accessibility Statement</div>
+
+
+
+                    </div>
+
+                </div>
+                <div className=" flex justify-between mob:flex-col tab:flex-col flex-row py-5">
+
+
+                    <img className="text-xs mob:w-20 w-24 mx-16 py-2" src="/logo-udemy-inverted.svg" />
+
+
+
+                    <div className="text-white text-xs px-10 mx-6 py-2">© 2022 Udemy, Inc.</div>
+
+                </div>
+
+            </div>
+        </div>
